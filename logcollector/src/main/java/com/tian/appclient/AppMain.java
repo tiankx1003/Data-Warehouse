b4408c7601da8923593dbee279276acfcaabc233
Tiankx1003 <tiankx1003@gmail.com>
2019-08-21 13:10:07 +0800

    log-collector

---
+++ logcollector/src/main/java/com/tian/appclient/AppMain.java
@@ -0,0 +1,9 @@
+package com.tian.appclient;
+
+/**
+ * @author JARVIS
+ * @version 1.0
+ * 2019/8/21 12:44
+ */
+public class Appmain {
+}
---
+++ logcollector/src/main/java/com/tian/bean/AppActive_foreground.java
@@ -0,0 +1,9 @@
+package com.tian.bean;
+
+/**
+ * @author JARVIS
+ * @version 1.0
+ * 2019/8/21 12:56
+ */
+public class AppActive_foreground {
+}
